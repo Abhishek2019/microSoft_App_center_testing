fdb7db6d7e62ac3676ee5c6193f799708b35ca7e
Abhishek2019 <abhishekw13@gmail.com>
2018-03-25 17:44:21 +0530

    gradle builded 1

--- geo31/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ geo31/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp7e4f66db0a2a47cfbd145d186bd326d0://localhost:80";
 
 }
